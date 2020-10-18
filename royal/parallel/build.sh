4f4a2150031fc72c3a3f145d4508c3e43de692d4
chiarcos <christian.chiarcos@web.de>
2020-10-18 15:21:50 +0200

    added Sumerian text to parallel corpus

--- royal/parallel/build.sh
+++ royal/parallel/build.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
-# create conll files from atf files
+# for every eng file, create a Sumerian txt file from the atf
+
+if [ ! -e sux ]; then
+	mkdir sux;
+fi;
+
+for file in eng/*; do
+	base=`basename $file | sed s/'_.*'//`;
+	atf=`ls ../atf/$basename*atf | grep -m 1 .`;
+	tgt=sux/`basename $file | sed s/'\..*'//g`.txt
+	echo -n $tgt' ' 1>&2;
+	if [ -s $tgt ]; then
+		echo found 1>&2;
+	else
+		if [ -e $atf ]; then
+			egrep '^[1-9][0-9]*\s*\.\s*' $atf | \
+			perl -pe 's/^[0-9]+\s*\.\s*//;' > $tgt
+			if [ -s $tgt ]; then
+				echo ok 1>&2
+			else
+				echo failed 1>&2
+			fi;
+		else
+			echo failed' ('$atf' not found)' 1>&2
+		fi;
+	fi;
+done;
 
