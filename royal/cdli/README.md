b1f2be7697799339f74b08882accd3a69b5c5a39
chiarcos <christian.chiarcos@web.de>
2020-10-28 14:24:36 +0100

    aft => cdli/atf; parallel folder reset to core data

---
+++ royal/cdli/README.md
@@ -0,0 +1,11 @@
+# Ur-III royal subcorpus, CDLI/MTAAC source files
+
+`atf` from daily dump, Oct 8, 2020 (https://cdli.ucla.edu/bulk_data/), names changed to P_Q schema
+
+`conll` from MTAAC Ur III corpus, https://github.com/cdli-gh/mtaac_cdli_ur3_corpus, names changed to P_Q schema
+
+Note that incomplete translations have been corrected in the atf files. These have not been fed back into CDLI, for two reasons: 
+
+(1) Some lines in the ATF files are correct but have been removed here because they interfered with the extraction of translations. Feeding this information back into CDLI would mean to remove valid information.
+
+(2) All additions have been done by analogy with other translations in the Ur-III corpus, but without the consultation of an expert. This means, for example, that the correct translation variant of a word may actually differ from the one assumed here. As an example, szagina is translated variously as "general", "military governor", or "governor-general" (these interpretations differ with respect to whether the respective rank is relative to a military unit or relative to a specific region), and the respective translations require a deeper understanding of the context, e.g., the role of the individual described by the term in *other* texts. However, for the sake of annotation projection, these interpretation variants will not have an impact on the syntactic relations. 
--- royal/parallel/build.sh
+++ royal/parallel/build.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
-# for every eng file, create a Sumerian txt file from the atf
+# PRE: eng/ directory with atf-based English translations (*.txt) or ETCSRI-based English translations (*.etcsri.txt, only if no ATF-based translation)
+# PRE: ../cdli/atf/ directory with CDLI atfs
 
+# POST: for every eng file, create a Sumerian txt file from the atf
+
+#########################
+# create sux/ directory #
+#########################
 if [ ! -e sux ]; then
 	mkdir sux;
 fi;
 
 for file in eng/*; do
 	base=`basename $file | sed s/'_.*'//`;
-	atf=`ls ../atf/$basename*atf | grep -m 1 .`;
+	atf=`ls ../cdli/atf/$base*atf | grep -m 1 .`;
 	tgt=sux/`basename $file | sed s/'\..*'//g`.txt
 	echo -n $tgt' ' 1>&2;
 	if [ -s $tgt ]; then
 		echo found 1>&2;
 	else
 		if [ -e $atf ]; then
-			egrep '^[1-9][0-9]*\s*\.\s*' $atf | \
-			perl -pe 's/^[0-9]+\s*\.\s*//;' > $tgt
+			egrep '^[a-z]?[0-9][0-9a-z\-]*[^0-9a-zA-Z\.\s]*\s*\.\s*' $atf | \
+			perl -pe 's/^[a-z]?[0-9][0-9a-z\-]*[^0-9a-zA-Z\.\s]*\s*\.\s*//;' > $tgt
 			if [ -s $tgt ]; then
 				echo ok 1>&2
 			else
@@ -27,3 +33,71 @@ for file in eng/*; do
 	fi;
 done;
 
+###############################################
+# one document per line for files from ETCSRI #
+###############################################
+# ETCSRI translations are paragraph-aligned only, but we don't have the corresponding breaks in the CDLI-ATF.
+# We thus treat the entire document as a single sentence.
+# GIZA++ does have a sentence length limit of 100 words. No Ur III ETSCRI document (among those without
+# translations in the ATF) exceeds that. (But note that this won't work for ETSCRI as a whole.)
+# CDLI ATFs are aligned for every line.
+
+for file in eng/*etcsri*txt; do
+	base=`basename $file | sed s/'_.*'//`;
+	sux=sux/`basename $file | sed s/'\..*'//g`.txt
+	sux_cdli=sux/`basename $file | sed s/'\..*'//g`.cdli.txt
+	tgt=eng/`basename $sux`;
+	echo -n $tgt' ' 1>&2;
+	if [ -s $tgt ]; then
+		echo found 1>&2;
+	else
+		(cat $file | \
+		perl -pe 's/^\(([a-zA-Z]+\s*)?[0-9]+[^a-zA-Z0-9\s]*\s*\)\s+//g; s/\s+/ /g;' | \
+		perl -pe 's/  +/ /g;'
+		echo) > $tgt
+		if [ -s $tgt ]; then
+			echo ok 1>&2;
+		else 
+			echo failed 1>&2;
+		fi;
+	fi;
+	echo -n normalize $sux' ' 1>&2;
+	if [ -e $sux_cdli ]; then
+		echo skipped 1>&2;
+	else
+		mv $sux $sux_cdli
+		(cat $sux_cdli | \
+		perl -pe 's/\s+/ /g;' | \
+		perl -pe 's/  +/ /g;';
+		echo) > $sux
+		if [ -s $sux ]; then
+			echo ok 1>&2;
+		else
+			echo failed 1>&2;
+		fi;
+	fi;
+done;
+
+######################
+# validate alignment #
+######################
+
+for sux in sux/*; do
+	eng=eng/`basename $sux`;
+	sux_cdli=sux/`basename $sux | sed s/'\..*'//g`.cdli.txt
+	if [ -s $eng ]; then
+		if [ `cat $sux | wc -l` = `cat $eng | wc -l` ]; then 
+			echo $sux $eng valid >&/dev/null; #1>&2;
+		else
+			echo warning: $sux `cat $sux | wc -l` '('`cat $sux | wc -w`')' but $eng `cat $eng | wc -l` '('`cat $eng | wc -w`')' 1>&2;
+			if [ ! -e sux/tmp ]; then
+				mkdir sux/tmp
+			fi;
+			mv $sux sux/tmp
+			mv $eng sux/tmp
+			if [ -e $sux_cdli ]; then
+				mv $sux_cdli sux/tmp
+			fi;
+		fi;
+	fi;
+done;
